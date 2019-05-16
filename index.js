8d80102ecba02fed5baca30b7be499f53a9cb5ad
Danny <windate3411@gmail.com>
2019-05-16 11:22:44 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,129 @@
+(function () {
+  const BASE_URL = 'https://movie-list.alphacamp.io'
+  const INDEX_URL = BASE_URL + '/api/v1/movies/'
+  const POSTER = BASE_URL + '/posters/'
+  const data = []
+  const pagination = document.getElementById('pagination')
+  const ITEM_PER_PAGE = 12
+  let paginationData = []
+
+  axios.get(`https://movie-list.alphacamp.io/api/v1/movies/`)
+    .then((response) => {
+      data.push(...response.data.results)
+      displayDataList(data)
+      getTotalPages(data)
+      getPageData(1, data)
+    })
+    .catch((err) => console.log(err))
+
+  const data_panel = document.getElementById('data-panel')
+  data_panel.addEventListener('click', (event) => {
+    if (event.target.matches('.btn-show-movie')) {
+      showMovie(event.target.dataset.id)
+    } else if (event.target.matches('.btn-add-favorite')) {
+      addFavoriteItem(event.target.dataset.id)
+      console.log(event.target.dataset.id)
+    }
+  })
+  function displayDataList(data) {
+    let htmlcontent = ''
+    data.forEach((item, index) => {
+      htmlcontent += `
+        <div class="col-sm-3">
+          <div class="card mb-2">
+            <img class="card-img-top " src="${POSTER}${item.image}" alt="Card image cap">
+            <div class="card-body movie-item-body">
+              <h6 class="card-title">${item.title}</h5>
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${item.id}">More</button>
+              <button class="btn btn-info btn-add-favorite" data-id="${item.id}">+</button>
+            </div>
+          </div>
+        </div>
+      `
+    })
+    data_panel.innerHTML = htmlcontent
+  }
+  function showMovie(id) {
+    // get elements
+    const modalTitle = document.getElementById('show-movie-title')
+    const modalImage = document.getElementById('show-movie-image')
+    const modalDate = document.getElementById('show-movie-date')
+    const modalDescription = document.getElementById('show-movie-description')
+
+    // set request url
+    const url = INDEX_URL + id
+    console.log(url)
+
+    // send request to show api
+    axios.get(url).then(response => {
+      const data = response.data.results
+      console.log(data)
+
+      // insert data into modal ui
+      modalTitle.textContent = data.title
+      modalImage.innerHTML = `<img src="${POSTER}${data.image}" class="img-fluid" alt="Responsive image">`
+      modalDate.textContent = `release at : ${data.release_date}`
+      modalDescription.textContent = `${data.description}`
+    })
+  }
+
+  // search-bar
+  const searchBtn = document.getElementById('submit-search')
+  const searchInput = document.getElementById('search')
+
+  // listen to search btn click event
+  searchBtn.addEventListener('click', event => {
+    let results = []
+    event.preventDefault()
+
+    const regex = new RegExp(searchInput.value, 'i')
+
+    results = data.filter(movie => movie.title.match(regex))
+    console.log(results)
+    displayDataList(results)
+    getTotalPages(results)
+    getPageData(1, results)
+  })
+  //my favorite
+  function addFavoriteItem(id) {
+    const list = JSON.parse(localStorage.getItem('favoriteMovies')) || []
+    const movie = data.find(item => item.id === Number(id))
+
+    if (list.some(item => item.id === Number(id))) {
+      alert(`${movie.title} is already in your favorite list.`)
+    } else {
+      list.push(movie)
+      alert(`Added ${movie.title} to your favorite list!`)
+    }
+    localStorage.setItem('favoriteMovies', JSON.stringify(list))
+  }
+  // 下方分頁列
+  function getTotalPages(data) {
+    let totalPages = Math.ceil(data.length / ITEM_PER_PAGE) || 1
+    let pageItemContent = ''
+    for (let i = 0; i < totalPages; i++) {
+      pageItemContent += `
+        <li class="page-item">
+          <a class="page-link" href="javascript:;" data-page="${i + 1}">${i + 1}</a>
+        </li>
+      `
+    }
+    pagination.innerHTML = pageItemContent
+  }
+  //分頁列事件
+  pagination.addEventListener('click', event => {
+    console.log(event.target.dataset.page)
+    if (event.target.tagName === 'A') {
+      getPageData(event.target.dataset.page)
+    }
+  })
+  //取得分頁資料
+  function getPageData(pageNum, data) {
+    paginationData = data || paginationData
+    let offset = (pageNum - 1) * ITEM_PER_PAGE
+    let pageData = paginationData.slice(offset, offset + ITEM_PER_PAGE)
+    displayDataList(pageData)
+  }
+})()
